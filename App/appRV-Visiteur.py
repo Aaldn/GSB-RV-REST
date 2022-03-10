4d16e17c8b13156615781046997fc3fc66e3b589
aaldn <aladdine.abdou.sio@gmail.com>
2022-03-10 21:09:36 +0100

    Création d'un certificat SSL

--- App/appRV-Visiteur.py
+++ App/appRV-Visiteur.py
@@ -121,3 +121,5 @@ def addEchantillonsOfferts(matricule, numRapport):
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+    # Run
+    # flask run -h <host ip> -p 5000 --cert=cert.pem --key=key.pem
