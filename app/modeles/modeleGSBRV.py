8a0833aec76548d7d886ee42953c5b184fbc8165
aaldn <aladdine.abdou.sio@gmail.com>
2022-03-14 18:24:54 +0100

    Modification du script de création de la base de données

--- app/modeles/modeleGSBRV.py
+++ app/modeles/modeleGSBRV.py
@@ -17,7 +17,7 @@ def getConnexionBD():
                 'password': 'root',
                 'host': 'db',
                 'port': '3306',
-                'database': 'gsb_rv_visiteur'
+                'database': 'gsb_rv'
             }
             connexionBD = mysql.connector.connect(**config)
         return connexionBD
